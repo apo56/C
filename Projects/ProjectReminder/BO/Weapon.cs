1329bd7f13394b3a990f7ee5af85004c9a417e96
pierre <pierre.ledorze56@gmail.com>
2018-03-20 16:36:15 +0100

    ajout library utile ProjectReminder

---
+++ Projects/ProjectReminder/BO/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BO
+{
+    public class Weapon
+    {
+        public int Id { get; set; }
+        public int DegatMin { get; set; }
+        public int DegatMax { get; set; }
+        public string Nom { get; set; }
+        public Warrior Warrior { get; set; }
+    }
+}
